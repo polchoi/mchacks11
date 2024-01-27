bdc829cb7a6df61aa34c3c5937d8d7815a30a9a2
CeDaGonCa <157912101+CeDaGonCa@users.noreply.github.com>
2024-01-27 17:03:05 -0500

    Update main.py
    trial

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"McHacks 11! By Cesar, Karman, Oliver, Yoon Sun"}
-
+#asdw
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
