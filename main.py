9de83e58cbea890521a9a67e2803c2d5bb5729c5
Shubkarman Walia <shubhkarmansingh27sk@gmail.com>
2024-01-27 17:19:32 -0500

    Practice

--- main.py
+++ main.py
@@ -6,10 +6,22 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"McHacks 11! By Cesar, Karman, Oliver, Yoon Sun"}
+def read_root(): 
+    return {"McHacks 11! By Cesar, Karma n, Oliver, Yoon Sun"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+students = {
+    1: {
+        "Name": "John",
+        "Age": 17,
+        "Id": 1
+    }
+}
+
+@app.get("/get-student/{Id}")
+def get_studetn(Id: int):
+    return students[Id]
