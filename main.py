e0432a0685cd2c711f23ae3d25085cfd492c93c3
CeDaGonCa <157912101+CeDaGonCa@users.noreply.github.com>
2024-01-27 19:04:20 -0500

    Update main.py
    Fixed a wrong implementation

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 from typing import Union
 
 from fastapi import FastAPI
+
 app = FastAPI()
+
 
 # MINE
 class CLIENT:
-    def __init__(self):
+    def __init__(self, name: str):
+        self.name = name
         self.expensesReport = []
         self.spent = 0
+
+
 class EXPENSES:
-    def __init__(self,Name:str,Price:float,Category:str,Date:str)
+    def __init__(self, Name: str, Price: float, Category: str, Date: str):
         self.Name = Name
         self.Price = Price
         self.Category = Category
         self.Date = Date
+
+
 Clients = []
+
+
 @app.get("/")
 def read_root():
     return {"McHacks 11! By Cesar, Karman, Oliver, Yoon Sun"}
@@ -23,11 +32,15 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
 @app.get("/items")
-def new_client(name:str,price:float,category:str,date:str):
-    Clients.append(CLIENT(name,price,category,date))
+def new_client(name: str):
+    Clients.append(CLIENT(name))
     return None
+
+
 @app.get("/items/{item_id}")
-def new_expense(exp: expensesReport, cli: client ):
+def new_expense(exp: EXPENSES, cli: CLIENT):
     cli.expensesReport.append(exp)
     return None
